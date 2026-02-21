30a424964dd4438165b5874e2da7e33ebf549c1a
prakashyari <prakashyari918@gmail.com>
2026-02-21 11:00:37 +0545

    updated main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 secret_number =random.randint(1,10)
-print("Welcome to the number guessing game!")
-print("I have selected a number between 1 and 10. Can you guess it?")
+print("Welcome to the number guessing game!guys")
+print("I have selected a number between 1 and 10")
 while True:
     guess = int(input("Enter your guess: "))
     if guess < secret_number:
